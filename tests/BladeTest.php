38df2257cfd5213170c2843c2214d49a7425ac47
Daison Carino <daison12006013@gmail.com>
2016-03-21 09:31:25 +0000

    Enhance the test suite by checking the added custom directive

--- tests/BladeTest.php
+++ tests/BladeTest.php
@@ -2,38 +2,87 @@
 
 use Jenssegers\Blade\Blade;
 
-class BladeTest extends PHPUnit_Framework_TestCase {
+class BladeTest extends PHPUnit_Framework_TestCase
+{
+    private $blade;
+
+    public function setUp()
+    {
+        $this->blade = new Blade('tests/views', 'tests/cache');
+
+        $this->blade->compiler()->directive('datetime', function($expression) {
+            return "<?php echo with({$expression})->format('F d, Y g:i a'); ?>";
+        });
+    }
 
     public function testBasic()
     {
-        $blade = new Blade('tests/views', 'tests/cache');
-
-        $output = $blade->make('basic')->render();
+        $output = $this->blade->make('basic')->render();
         $this->assertEquals('hello world', trim($output));
     }
 
     public function testVariables()
     {
-        $blade = new Blade('tests/views', 'tests/cache');
-
-        $output = $blade->make('variables', ['name' => 'John Doe'])->render();
+        $output = $this->blade->make('variables', ['name' => 'John Doe'])->render();
         $this->assertEquals('hello John Doe', trim($output));
     }
 
     public function testNonBlade()
     {
-        $blade = new Blade('tests/views', 'tests/cache');
-
-        $output = $blade->make('plain')->render();
+        $output = $this->blade->make('plain')->render();
         $this->assertEquals('this is plain php', trim($output));
     }
 
     public function testRenderAlias()
     {
-        $blade = new Blade('tests/views', 'tests/cache');
-
-        $output = $blade->render('basic');
+        $output = $this->blade->render('basic');
         $this->assertEquals('hello world', trim($output));
     }
 
+    public function testExtenderBlade()
+    {
+        $users = require __DIR__.'/data/users.php';
+
+        $blade_name = 'extender';
+
+        $output = $this->blade
+            ->make($blade_name, $users)
+            ->render();
+
+        # uncomment if you want to update the sample output
+        // $this->write($output, $blade_name);
+
+        $this->assertEquals(
+            $output,
+            $this->read($blade_name)
+        );
+    }
+
+    /**
+     * Html Writer on sample_output folder
+     *
+     * @param string $output The HTML output
+     * @param string $blade_name The blade file name/path
+     *
+     * @return void
+     */
+    private function write($output, $blade_name)
+    {
+        $file_path = __DIR__.'/sample_output/'.$blade_name.'.html';
+
+        file_put_contents($file_path, $output);
+    }
+
+    /**
+     * HTML Reader on sample_output folder
+     *
+     * @param string $blade_name The blade file name/path
+     * @return string
+     */
+    private function read($blade_name)
+    {
+        $file_path = __DIR__.'/sample_output/'.$blade_name.'.html';
+
+        return file_get_contents($file_path);
+    }
 }
---
+++ tests/views/extender.blade.php
@@ -0,0 +1,66 @@
+@extends('main.layout')
+
+@section('title')
+Test Title
+@endsection
+
+@section('content')
+
+{{-- you must get 'Default' --}}
+{{ isset($name) ? $name : 'Default' }}<br>
+{{ $name or 'Still Default' }}<br>
+
+{{-- it must strip out this XSS sample --}}
+{{ '<script type="text/javascript">alert("Hacked!");</script>' }}<br>
+
+{{-- this must be bold --}}
+<?php $name = '<strong>John Doe</strong>' ?>
+Hello, {!! $name !!}.<br>
+
+{{-- using include directive --}}
+@include('variables', ['name' => $name]) <br>
+
+{{-- @each('variables', $users, 'name') <br> --}}
+
+<?php
+$dateObj = new DateTime('2017-01-01 23:59:59');
+?>
+
+{{-- using object with() --}}
+<?php echo with($dateObj)->format('m/d/Y H:i:s'); ?> <br>
+
+{{-- using custom directive --}}
+@datetime($dateObj) <br>
+
+{{-- using unless, reversing 'true' --}}
+@unless ($auth_check = false)
+    You are not signed in.
+@endunless
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <table class="table table-bordered">
+                @if (count($users) > 0)
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                    @foreach ($users as $user)
+                    <tr>
+                        <td>{{ $user['id'] }}</td>
+                        <td>{{ $user['name'] }}</td>
+                        <td>{{ $user['email'] }}</td>
+                    </tr>
+                    @endforeach
+                @else
+                    <tr>
+                        <td colspan="3">No users found!</td>
+                    </tr>
+                @endif
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
