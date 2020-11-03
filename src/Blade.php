865a046a2d722534872e458b7672e63372c28866
bangnokia <bangnokia@gmail.com>
2020-11-03 22:12:12 +0700

    remove singletonIf

--- src/Blade.php
+++ src/Blade.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Facade;
 use Illuminate\View\Compilers\BladeCompiler;
 use Illuminate\View\Factory;
 use Illuminate\View\ViewServiceProvider;
+use function Couchbase\defaultDecoder;
 
 class Blade implements FactoryContract
 {
@@ -128,14 +129,15 @@ class Blade implements FactoryContract
             ];
         }, true);
 
-        $this->container->singletonIf(FactoryContract::class, function () {
+        $this->container->singleton(FactoryContract::class, function () {
             return $this;
         });
 
-        $this->container->singletonIf(Application::class, function () {
+        $this->container->singleton(Application::class, function () {
             return new class {
                 public function getNamespace() {
-                    return 'App\\';
+                    $composer = json_decode(file_get_contents('composer.json'), true);
+                    return count($composer['autoload']['psr-4'] ?? []) ? array_keys($composer['autoload']['psr-4'])[0] : null;
                 }
             };
         });
